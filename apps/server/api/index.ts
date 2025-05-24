62dc3980f26bdcfd83f8f50f851e8509bc3126ed
Mochamad Rafli <raflitea123@gmail.com>
2025-05-24 15:46:20 +0700

    chore: change build command

--- apps/server/api/index.ts
+++ apps/server/api/index.ts
@@ -1,5 +1,6 @@
 import { handle } from "@hono/node-server/vercel";
 // eslint-disable-next-line ts/ban-tscomment
+// @ts-expect-error
 import app from "../dist/index.js";
 
 export default handle(app);
