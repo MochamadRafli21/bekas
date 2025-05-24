3840e2e603aa0b3b968a5b989baeb52e99237bda
Mochamad Rafli <raflitea123@gmail.com>
2025-05-24 19:23:19 +0700

    chore: update tsconfig explicit file ext

--- apps/server/src/trpc/index.ts
+++ apps/server/src/trpc/index.ts
@@ -1,5 +1,5 @@
-import { router } from "./router.js";
-import { productRouter } from "./product.js";
+import { router } from "./router";
+import { productRouter } from "./product";
 
 export const appRouter = router({
   product: productRouter,
