ae96a8c4e4059526e8cf8ef9620550f0ce6c3823
Mochamad Rafli <raflitea123@gmail.com>
2025-05-24 15:26:51 +0700

    chore: move edge to node runtime

--- apps/server/api/index.ts
+++ apps/server/api/index.ts
@@ -1,15 +1,5 @@
-import { handle } from "hono/vercel";
+import { handle } from "@hono/node-server/vercel";
 // eslint-disable-next-line ts/ban-tscomment
-// @ts-expect-error
 import app from "../dist/index.js";
-export const runtime = "edge";
 
-const handler = handle(app);
-
-export const GET = handler;
-export const POST = handler;
-export const PATCH = handler;
-export const PUT = handler;
-export const OPTIONS = handler;
-export const DELETE = handler;
-export const HEAD = handler;
+export default handle(app);
