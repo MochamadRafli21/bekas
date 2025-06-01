afa307d6821355abf8e3c8130e2153eeeefc4241
Mochamad Rafli <raflitea123@gmail.com>
2025-06-01 20:46:16 +0700

    fix: missing api file

--- apps/server/api/index.ts
+++ apps/server/api/index.ts
@@ -1,6 +1,6 @@
 import { handle } from "@hono/node-server/vercel";
 // eslint-disable-next-line
 // @ts-ignore
-import app from "../dist/src/index.js";
+import app from "../src/index.js";
 
 export default handle(app);
